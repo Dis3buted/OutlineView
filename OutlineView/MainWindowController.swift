71396349b799ed74259ee71c43da5c572691e876
Dis3buted <peter.ahlberg@vikingline.fi>
2017-12-02 15:55:47 +0200

    Update MainWindowController.swift
    Fix on delegate and datasource

--- OutlineView/MainWindowController.swift
+++ OutlineView/MainWindowController.swift
@@ -31,32 +31,26 @@ class MainWindowController: NSWindowController {
 
 //MARK: - NSOutlineViewDelegate
 extension MainWindowController: NSOutlineViewDelegate {
-    func outlineView(_ outlineView: NSOutlineView, child index: Int, ofItem item: Any?) -> Any {
-        if let it = item as? FileItem {
-            return it.childAtIndex(index)!
-        }
-        return rootItem!
-    }
-    
-    func outlineView(_ outlineView: NSOutlineView, viewFor tableColumn: NSTableColumn?, item: Any) -> NSView? {
-        let view = outline.make(withIdentifier: "TextCell", owner: self) as? NSTableCellView
+  func outlineView(_ outlineView: NSOutlineView, viewFor tableColumn: NSTableColumn?, item: Any) -> NSView? {
+        let view = outlineView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: "TextCell"), owner: self) as? NSTableCellView
         if let it = item as? FileItem {
             if let textField = view?.textField {
                 textField.stringValue = it.displayName
             }
-        } 
+        }
         return view
     }
 }
 
 //MARK: - NSOutlineViewDataSource
 extension MainWindowController: NSOutlineViewDataSource {
-    func outlineView(_ outlineView: NSOutlineView, numberOfChildrenOfItem item: Any?) -> Int {
+   func outlineView(_ outlineView: NSOutlineView, numberOfChildrenOfItem item: Any?) -> Int {
         if let it = item as? FileItem {
             return it.count
         }
         return 1
     }
+    
     func outlineView(_ outlineView: NSOutlineView, isItemExpandable item: Any) -> Bool {
         if let it = item as? FileItem {
             if it.count > 0 {
@@ -64,5 +58,12 @@ extension MainWindowController: NSOutlineViewDataSource {
             }
         }
         return false
+    }
+    
+    func outlineView(_ outlineView: NSOutlineView, child index: Int, ofItem item: Any?) -> Any {
+        if let it = item as? FileItem {
+            return it.childAtIndex(index)!
+        }
+        return rootItem!
     }
 }
